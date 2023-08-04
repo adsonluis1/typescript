87323608e694f7716996265d54e592f2befb2855
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-08-03 22:02:48 -0300

    funções

---
+++ funções/index.js
@@ -0,0 +1,13 @@
+function somar(a, b) {
+    return a + b;
+}
+;
+console.log(somar(10, 10));
+function somar2(c, d) {
+    return c + d;
+}
+;
+console.log(somar2(10, 30));
+function semretorno() {
+    var constante = 'adson';
+}
---
+++ funções/index.ts
@@ -0,0 +1,16 @@
+function somar(a:number , b:number){
+    return a + b
+};
+
+console.log(somar(10,10));
+
+function somar2 (c:number , d:number):number{
+    return c + d
+};
+
+console.log(somar2(10 , 30));
+
+function semretorno():void{
+    const constante:string = 'adson' 
+}
+
