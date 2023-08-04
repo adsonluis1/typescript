9a89865485507a56c1312c97683671964194da99
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-08-03 21:34:03 -0300

    any

---
+++ any/index.js
@@ -0,0 +1,7 @@
+// (nn usar o any) mas ele serve pra deixar a especialização do type em aberto
+// não sei porq ta dando erro mas ta funcionando :)
+var variavel = 'adson';
+variavel = 18;
+variavel = true;
+variavel = ['adson', 'anne'];
+console.log(variavel);
---
+++ any/index.ts
@@ -0,0 +1,10 @@
+// (nn usar o any) mas ele serve pra deixar a especialização do type em aberto
+// não sei porq ta dando erro mas ta funcionando :)
+let variavel:any = 'adson'
+
+variavel = 18
+variavel = true
+variavel = ['adson' , 'anne']
+
+console.log(variavel)
+
