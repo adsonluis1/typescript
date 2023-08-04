501af8f2bcc84bea759d18cb7100af7eb15c8907
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-08-03 21:22:02 -0300

    tuplas

---
+++ aulão/tuplas/tuplas.js
@@ -0,0 +1,4 @@
+//é um array que eu vou determinar oq ele vai fzr 
+var array;
+array = ['adson', 18, true];
+console.log('');
---
+++ aulão/tuplas/tuplas.ts
@@ -0,0 +1,7 @@
+//é um array que eu vou determinar oq ele vai fzr 
+
+let array:[string,number,boolean]
+
+array= ['adson' , 18 , true]
+
+console.log(array)
